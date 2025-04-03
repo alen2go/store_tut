fbbc2f73fceec7d39ab75ccbb3cc4d3516ad1533
Alen <odisho_alen@hotmail.de>
2025-04-03 17:12:02 +0100

    Product and ProductController-class added. lombok added to pom

---
+++ src/main/java/de/odisho/store/Product.java
@@ -0,0 +1,10 @@
+package de.odisho.store;
+
+import lombok.Data;
+
+@Data
+public class Product {
+    private int prodId;
+    private String prodName;
+    private int price;
+}
---
+++ src/main/java/de/odisho/store/ProductController.java
@@ -0,0 +1,8 @@
+package de.odisho.store;
+
+public class ProductController {
+
+    public String getProducts() {
+        return "";
+    }
+}
