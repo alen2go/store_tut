df8d038f74382bd206b1f80ff0d2a9c91b2017dd
Alen <odisho_alen@hotmail.de>
2025-04-03 11:52:37 +0100

    removed some methods, to start clean and go on with the tutorial

--- src/main/java/de/odisho/store/HomeController.java
+++ src/main/java/de/odisho/store/HomeController.java
@@ -1,24 +1,13 @@
 package de.odisho.store;
 
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 
-@RestController
+@Controller
 public class HomeController {
-    @Value("${spring.application.name}")
-    private String appName;
-
     @RequestMapping("/")
     public String index() {
         return "index.html";
     }
-
-    @GetMapping("/string")
-    public String getStr() {
-        return "Hello, World!";
-    }
-
 }
