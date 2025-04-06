c082dd8f627490165845dd9b710e23fd7faf7583
Alen <odisho_alen@hotmail.de>
2025-04-06 17:53:39 +0100

    added GeneratedValue to id attribute

--- src/main/java/de/odisho/store/modle/Product.java
+++ src/main/java/de/odisho/store/modle/Product.java
@@ -6,6 +6,8 @@ import java.util.Date;
 import org.springframework.stereotype.Component;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -18,6 +20,7 @@ import lombok.NoArgsConstructor;
 @Entity
 public class Product {
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
     private String name;
     private String desc;
